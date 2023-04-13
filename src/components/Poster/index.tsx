394960496cc9fe6f267f3ef3836da5da0e32a59b
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-13 14:46:03 -0300

    Começa a criar o componente Poster

---
+++ src/components/Poster/index.tsx
@@ -0,0 +1,22 @@
+import {Text, View } from 'react-native'
+import React, { Component } from 'react'
+import styles from "./style"
+
+
+const Poster = ({pontos, participante}) => {
+
+    return (
+           
+      <View style={styles.container}>
+        <Text style={styles.participante}>{participante}</Text>
+        <View style={styles.pontuacao}>
+            <Text>{pontos}</Text>
+            <Text>pts</Text>
+        </View>                  
+       
+      </View>
+    )
+  
+}
+
+export default Poster
