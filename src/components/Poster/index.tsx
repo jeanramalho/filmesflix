4cbf819cc544706bbf2dadbda37ed605027a64f5
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-14 15:25:03 -0300

    Identa componente de poster

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -8,10 +8,7 @@ const Poster = ({pontos, participante}) => {
     return (
            
       <View style={styles.container}>
-                   <Image
-              source={{
-                uri: `https://m.media-amazon.com/images/I/81jEuP2Dn1L.jpg`,
-              }}
+        <Image source={{uri: `https://m.media-amazon.com/images/I/81jEuP2Dn1L.jpg`,}}
               resizeMode="cover"
               style={StyleSheet.absoluteFill}
             />    
